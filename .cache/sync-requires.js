fc8d68ac90291be6b705f2acfe2a9597d7102216
Tiffany Abraham <tony.calablaval@gmail.com>
2021-02-12 04:28:02 -0500

    Re-commit

---
+++ .cache/sync-requires.js
@@ -0,0 +1,14 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/tiffanyabraham/Development/2021/tobaccohut/tobacco-hut-website/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/tiffanyabraham/Development/2021/tobaccohut/tobacco-hut-website/src/pages/404.js"))),
+  "component---src-pages-about-js": hot(preferDefault(require("/Users/tiffanyabraham/Development/2021/tobaccohut/tobacco-hut-website/src/pages/about.js"))),
+  "component---src-pages-elements-js": hot(preferDefault(require("/Users/tiffanyabraham/Development/2021/tobaccohut/tobacco-hut-website/src/pages/elements.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/tiffanyabraham/Development/2021/tobaccohut/tobacco-hut-website/src/pages/index.js")))
+}
+
---
+++ content copy/blog/smoking-accesories/index.md
@@ -0,0 +1,13 @@
+---
+title: Smoking Accessories
+date: "2019-05-02"
+thumbnail: ./griinder_2.jpg
+description: A wide selection of smoking accessories including grinders, containers, and more!
+---
+
+
+<div class="kg-card kg-image-card kg-width-full">
+
+![Smoking Accesories](./griinder.jpg)
+
+</div>
