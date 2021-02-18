868e209deb02f8e56018582e8c10e2ca737b3495
Tiffany Abraham <tony.calablaval@gmail.com>
2021-02-18 05:16:14 -0500

    Rdmoved childimagesharp from elemenbts

--- src/pages/elements.js
+++ src/pages/elements.js
@@ -136,24 +136,15 @@ const ElementsPage = ({ data }, location) => {
           <hr />
           <h2 id="images">Images</h2>
           <figure className="kg-card kg-image-card">
-            <Img
-              fluid={data.smallPic.childImageSharp.fluid}
-              className="kg-image"
-            />
+
             <figcaption>Regular image</figcaption>
           </figure>
           <figure className="kg-card kg-image-card kg-width-wide">
-            <Img
-              fluid={data.medPic.childImageSharp.fluid}
-              className="kg-image"
-            />
+
             <figcaption>Large image</figcaption>
           </figure>
           <figure className="kg-card kg-image-card kg-width-full">
-            <Img
-              fluid={data.largePic.childImageSharp.fluid}
-              className="kg-image"
-            />
+
             <figcaption>Full bleed image</figcaption>
           </figure>
           <hr />
